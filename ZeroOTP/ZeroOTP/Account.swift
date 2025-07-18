b3db9b1022d20dd110b06416d40916d5d5ac2ff7
Trust-Worthy <jondeveloper0@gmail.com>
2025-07-17 19:43:59 -0600

    Creating my data model for the accounts.

---
+++ ZeroOTP/ZeroOTP/Account.swift
@@ -0,0 +1,19 @@
+//
+//  Account.swift
+//  ZeroOTP
+//
+//  Created by Trust-Worthy on 7/17/25.
+//
+
+import Foundation
+
+
+struct Account {
+    var accountName: String
+    var OTP: String
+    let base32Secret: String
+    
+    var dateCreated: Date
+    
+    
+}
---
+++ ZeroOTP/ZeroOTP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ZeroOTP
+//
+//  Created by Jonathan Bateman on 7/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    private var accounts: 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

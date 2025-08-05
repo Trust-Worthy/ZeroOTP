7f663b68b6a684d9e3c5897de6630029caa3037b
Trust-Worthy <jondeveloper0@gmail.com>
2025-08-04 21:07:15 -0600

    Finished the getCurrentPassword function.

--- ZeroOTP/ZeroOTP/Services/TOTPGenerator.swift
+++ ZeroOTP/ZeroOTP/Services/TOTPGenerator.swift
@@ -6,11 +6,33 @@
 //
 
 import SwiftOTP
+import UIKit
 
 struct TOTPGenerator {
     
-    let secret: String
-    let totpObject: TOTP
+    let secret: Data
+    let digits: Int = 6
+    let timeInterval: Int = 30
+    let algorithm: OTPAlgorithm = .sha1
+
     
+    // Computed property that returns a TOTP object with customized parameters.
+    // If all params aren't met for the constructor, a standard TOTP object is created with the default values.
+    var totpObject: TOTP? {
+        TOTP(secret: secret, digits: digits, timeInterval: timeInterval, algorithm: algorithm) ?? TOTP(secret: secret)
+    }
+        
+    
+    
+    // Retrieve the current password based on the current time.
+    // Return is as an optional in case something went wrong
+    func getCurrentPassword() -> String? {
+        
+        guard let totp = totpObject else {
+            return nil
+        }
+        
+        return totp.generate(time: Date.now) ?? "000000"
+    }
     
 }
